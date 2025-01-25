b60946bfebd828624645709b095a79695da712e5
Sasha_Kalych <144242090+Skalych@users.noreply.github.com>
2025-01-25 16:33:09 +0100

    Add files via upload

--- php.php
+++ php.php
@@ -1,42 +1,33 @@
 <?php
-// Налаштування підключення до бази даних
 $servername = "localhost";
-$username = "root";  // ваш логін для доступу до БД
-$password = "";  // ваш пароль для доступу до БД
-$dbname = "db";  // назва вашої бази даних
+$username = "root";
+$password = ""; 
+$dbname = "db";
 
-// Створення з'єднання
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Перевірка з'єднання
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Перевірка, чи було відправлено форму
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Отримуємо значення з форми
     $title = $_POST['title'];
     $genre = $_POST['genre'];
     $year = $_POST['year'];
     $rating = $_POST['rating'];
 
-    // Перевірка, чи всі поля заповнені
     if (empty($title) || empty($genre) || empty($year) || empty($rating)) {
-        echo "Всі поля повинні бути заповнені!";
+        echo "done!";
     } else {
-        // Підготовка SQL-запиту для вставки даних в таблицю games
         $sql = "INSERT INTO games (title, genre, year, rating) VALUES ('$title', $genre, $year, $rating)";
 
-        // Виконання запиту
         if ($conn->query($sql) === TRUE) {
-            echo "Гра успішно додана!";
+            echo "succes!";
         } else {
-            echo "Помилка: " . $sql . "<br>" . $conn->error;
+            echo "error: " . $sql . "<br>" . $conn->error;
         }
     }
 }
 
-// Закриваємо з'єднання
 $conn->close();
 ?>
